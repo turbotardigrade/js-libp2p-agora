3f1b0c40eb9a3458d6e3a8f86c8b7b4d37b9f5ce
Prashanth Chandra <coolshanth94@gmail.com>
2016-11-13 17:12:04 +0800

    Add data transfer example

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const co = require('bluebird-co')
 const libp2p = require('libp2p-ipfs')
 const multiaddr = require('multiaddr')
 const PeerInfo = Promise.promisifyAll(require('peer-info'))
+const pull = require('pull-stream')
 
 const setup = co.wrap(function* () {
   const [infoA, infoB] = yield [
@@ -23,18 +24,36 @@ const setup = co.wrap(function* () {
     nodeB.startAsync()
   ]
 
+  nodeB.handle('/echo/1.0.0', echo)
+
   const nodeBMultiaddrTCP = multiaddr((nodeB.peerInfo.multiaddrs[0].toString() +
     '/ipfs/' + nodeB.peerInfo.id.toB58String()
   ).replace('0.0.0.0', '127.0.0.0'))
   console.log(nodeBMultiaddrTCP)
 
-  yield nodeA.dialByMultiaddrAsync(nodeBMultiaddrTCP)
+  const conn = yield nodeA.dialByMultiaddrAsync(nodeBMultiaddrTCP, '/echo/1.0.0')
 
+  // After A dials B by multiaddr
+  // both nodes should have each other in their peer lists
   setTimeout(() => {
     console.log(nodeA.peerBook.getAll())
     console.log(nodeB.peerBook.getAll())
   }, 500)
+
+  // Use A's connection to B's /echo/1.0.0 to send a buffer
+  // This should bounce back to us
+  pull(
+    pull.values([Buffer('hey')]),
+    conn,
+    pull.collect((err, data) => {
+      console.log(data)
+    })
+  )
 })
+
+const echo = (protocol, conn) => {
+  pull(conn, conn)
+}
 
 setup()
 
